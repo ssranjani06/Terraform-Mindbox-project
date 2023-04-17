cc051d2dab0fc6ac0ac8e7b2fbe4710ec8cbae57
ssranjani06 <ssranjani06@gmail.com>
2023-04-17 11:24:01 +0530

    example.sh

--- example.sh
+++ example.sh
@@ -1,6 +1,10 @@
-!/bin/bash
-sudo su -root
+#!/bin/bash
+sudo su
+sudo su - root
 sudo apt-get update
 sudo apt-get install apache2 -y
+cd /var/www/html
+rm index.html
+cd /root
 git clone https://github.com/amolshete/card-website.git
-cp -rf card-website/* /var/www/html
+cp -rf card-website/* /var/www/html/
